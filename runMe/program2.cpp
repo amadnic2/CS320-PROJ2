6aa36b9e9511015b607e52b8e21b2773319b3f70
amadnic2 <35607877+amadnic2@users.noreply.github.com>
2020-04-29 20:58:22 -0400

    Update program2.cpp

--- runMe/program2.cpp
+++ runMe/program2.cpp
@@ -49,7 +49,7 @@ void DirectMap(char * file, int size){
 
 }
 
-void SetAssociative(char * file, int way){
+void SetAssociative(char * file, int way,){
     int hit = 0;
     int total = 0;
     int setIndex;
@@ -154,7 +154,116 @@ void SetAssociative(char * file, int way){
    }
     cout<<hit<<",";
     cout<<total<<"; ";
+}
 
+void HnC_fullAssociative(char * file){
+}
+
+void SetAssociativeWriteMiss(char * file, int way,){
+    int hit = 0;
+    int total = 0;
+    int setIndex;
+    int tag;
+    int sets = 512/way;
+    int setAssociative[sets][way];
+    int recency[sets][way];
+
+    for(int i = 0; i < sets; i++){
+      for(int j = 0; j < way; j++){
+        setAssociative[i][j] = -1;
+        recency[i][j] = 0;
+      }
+    }
+
+    //open file
+    ifstream infile(file);
+    string ldSt;
+    int address;
+    int logOf = log2(sets);
+
+    // The following loop will read a line at a time
+    while(infile >> ldSt >> std::hex >> address) {
+      address = address >> 5;
+      setIndex = address % (sets);
+      tag = address >> logOf;
+      bool isThere = 0;
+      int newPlace;
+      int oldRecency;
+
+
+      for(int i = 0; i < way; i++){
+        //check if there
+         if(setAssociative[setIndex][i] == tag){
+           hit++;
+	   oldRecency = recency[setIndex][i];
+           recency[setIndex][i] = way-1;
+           isThere = 1;
+           newPlace = i;
+	   break;
+        }
+      }
+
+      // update recency
+      if(isThere && ldSt == 'L'){
+        for(int i = 0; i < way; i++){
+          if(i != newPlace){
+          
+            if(recency[setIndex][i] > 0 && recency[setIndex][i] > oldRecency)  recency[setIndex][i]--;
+        }
+       }
+      }
+
+      if(!isThere){
+         bool replace = 1;
+         for(int i = 0; i < way; i++){
+           //check if room
+           if(setAssociative[setIndex][i] == -1){
+              recency[setIndex][i]= way-1;
+              setAssociative[setIndex][i] = tag;
+              replace = 0;
+              newPlace = i;
+              break;
+           }
+         }
+
+         //update recency
+         if(!replace){
+           for(int i = 0; i < way; i++){
+             if(i == newPlace) continue;
+             else{
+               if(recency[setIndex][i] > 0)  recency[setIndex][i]--;
+             }
+           }
+         }
+
+         if(replace){
+         //find replacement
+          int leastR = way;
+          for(int i= 0; i < way; i++){
+            if(recency[setIndex][i] < leastR){
+              leastR = recency[setIndex][i];
+              newPlace = i;
+           }
+          }
+
+          //set replacement tag and recency. uses first least recenct slot
+          for(int i = 0; i < way; i++){
+            if(i == newPlace){
+              setAssociative[setIndex][i] =tag;
+              recency[setIndex][i] =way -1;
+            }
+
+            else{
+              if(recency[setIndex][i] > 0)  recency[setIndex][i]--;
+            }
+
+          }
+        }
+     }
+     total++;
+   }
+    cout<<hit<<",";
+    cout<<total<<"; ";
 }
 
 int main(int argc, char * argv[]){
@@ -167,4 +276,13 @@ int main(int argc, char * argv[]){
   SetAssociative(argv[1],4);
   SetAssociative(argv[1],8);
   SetAssociative(argv[1], 16);
+  cout<<"\n";
+  SetAssociative(argv[1], 512);
+  cout<<"\n";
+  SetAssociativeWriteMiss(argv[1], 2);
+  SetAssociativeWriteMiss(argv[1],4);
+  SetAssociativeWriteMiss(argv[1],8);
+  SetAssociativeWriteMiss(argv[1], 16);
+  
+   
 }
